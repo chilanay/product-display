43e038c923598b9e95dd5e7d6ddbfee4ed06dd99
Chilanay <chilanay.hajisoy@gmail.com>
2023-12-01 21:36:39 +0400

    changed

--- fetch.js
+++ fetch.js
@@ -1,21 +1,38 @@
-const apiUrl = 'https://dummyjson.com/products';
+function getInfo (){
+    navigator.geolocation.getCurrentPosition((position) => {
+        
+        let lon = position.coords.longitude;
+        let lat = position.coords.latitude;
 
-// Function to handle errors during fetch
-const handleErrors = (response) => {
-    if (!response.ok) {
-        throw new Error(`Error: ${response.status} - ${response.statusText}`);
-    }
-    return response.json();
-};
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
 
-// Fetch data from the API
-fetch(apiUrl)
-    .then(handleErrors)
-    .then((data) => {
-        // Handle the data as needed
-        console.log('Fetched data:', data);
+         console.log(data);
+         weatherInfo(data);
+         
+        
+        })
+
     })
-    .catch((error) => {
-        // Handle errors during fetch
-        console.error('Fetch error:', error.message);
-    });
+}
+
+// const apiUrl = 'https://dummyjson.com/products';
+
+// // Function to handle errors during fetch
+// const handleErrors = (response) => {
+//     if (!response.ok) {
+//         throw new Error(`Error: ${response.status} - ${response.statusText}`);
+//     }
+//     return response.json();
+// };
+
+// // Fetch data from the API
+// fetch(apiUrl)
+//     .then(handleErrors)
+//     .then((data) => {
+//         // Handle the data as needed
+//         console.log('Fetched data:', data);
+//     })
+//     .catch((error) => {
+//         // Handle errors during fetch
+//         console.error('Fetch error:', error.message);
+//     });
